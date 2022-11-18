453ec171827a2b3ec4466a60ec131c4eb6c3c49a
geoshavel <geoshavel@gmail.com>
2022-11-18 13:52:04 +0200

    init

--- src/js/components/Sketch.js
+++ src/js/components/Sketch.js
@@ -1,26 +1,27 @@
 /* eslint-disable no-unreachable */
-import * as THREE from 'three';
+import * as T from 'three';
 import dat from 'dat.gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 // import fragment from './shader/fragment.glsl';
 // import vertex from '../../shader/vertex.glsl';
+import map from './earth.jpg';
 
 export default class Sketch {
 	constructor(options) {
-		this.scene = new THREE.Scene();
+		this.scene = new T.Scene();
 
 		this.container = options.dom;
 		this.width = this.container.offsetWidth;
 		this.height = this.container.offsetHeight;
-		this.renderer = new THREE.WebGLRenderer();
+		this.renderer = new T.WebGLRenderer();
 		this.renderer.setPixelRatio(window.devicePixelRatio);
 		this.renderer.setSize(this.width, this.height);
 		this.renderer.setClearColor(0xeeeeee, 1);
-		this.renderer.outputEncoding = THREE.sRGBEncoding;
+		this.renderer.outputEncoding = T.sRGBEncoding;
 
 		this.container.appendChild(this.renderer.domElement);
 
-		this.camera = new THREE.PerspectiveCamera(
+		this.camera = new T.PerspectiveCamera(
 			70,
 			window.innerWidth / window.innerHeight,
 			0.001,
@@ -29,7 +30,7 @@ export default class Sketch {
 
 		// var frustumSize = 10;
 		// var aspect = window.innerWidth / window.innerHeight;
-		// this.camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, -1000, 1000 );
+		// this.camera = new T.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, -1000, 1000 );
 		this.camera.position.set(0, 0, 2);
 		this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 		this.time = 0;
@@ -66,7 +67,7 @@ export default class Sketch {
 	}
 
 	loadObjects() {
-		const loader = new THREE.FileLoader();
+		const loader = new T.FileLoader();
 
 		const fragment = new Promise((resolve, reject) => {
 			loader.load(
@@ -104,28 +105,128 @@ export default class Sketch {
 	addObjects() {
 		let that = this;
 
-		this.material = new THREE.ShaderMaterial({
+		this.shaderMaterial = new T.ShaderMaterial({
 			extensions: {
 				derivatives: '#extension GL_OES_standard_derivatives : enable',
 			},
-			side: THREE.DoubleSide,
+			side: T.DoubleSide,
 			uniforms: {
 				time: { type: 'f', value: 0 },
-				resolution: { type: 'v4', value: new THREE.Vector4() },
+				resolution: { type: 'v4', value: new T.Vector4() },
 				uvRate1: {
-					value: new THREE.Vector2(1, 1),
+					value: new T.Vector2(1, 1),
 				},
 			},
 			// wireframe: true,
-			// transparent: true,
+			transparent: true,
 			vertexShader: this.vertex,
 			fragmentShader: this.fragment,
 		});
 
-		this.geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
+		this.material = new T.MeshBasicMaterial({
+			map: new T.TextureLoader().load(map),
+		});
 
-		this.plane = new THREE.Mesh(this.geometry, this.material);
-		this.scene.add(this.plane);
+		this.geometry = new T.SphereGeometry(1, 30, 30);
+
+		this.planet = new T.Mesh(this.geometry, this.material);
+		this.scene.add(this.planet);
+
+		const createMesh = (color) => {
+			return new T.Mesh(
+				new T.SphereGeometry(0.02, 20, 20),
+				new T.MeshBasicMaterial({
+					color,
+				}),
+			);
+		};
+
+		const points = [
+			{
+				name: 'Kiyv',
+				color: 0xff0000,
+				lat: 50.450001,
+				lng: 30.523333,
+			},
+			{
+				name: 'Washington DC',
+				color: 0x00ff00,
+				lat: 38.8951,
+				lng: -77.0364,
+			},
+			{
+				name: 'Pekin',
+				color: 0x00ff00,
+				lat: 40.568459,
+				lng: 89.643028,
+			},
+			{
+				name: 'Kyoto',
+				color: 0xff0000,
+				lat: 35.011665,
+				lng: 135.768326,
+			},
+		];
+
+		const { sin, cos, PI } = Math;
+
+		const getPosition = ({ lat, lng }) => {
+			const radian = (PI / 180);
+			const phi = (90 - lat) * radian;
+			const theta = (lng + 180) * radian;
+
+			const sinPhi = sin(phi);
+			const cosPhi = cos(phi);
+			const sinTheta = sin(theta);
+			const cosTheta = cos(theta);
+
+			return [
+				-(sinPhi * cosTheta), // x
+				cosPhi, // y
+				sinPhi * sinTheta, // z
+			];
+		};
+
+		points.forEach((
+			{
+				color,
+			},
+			i,
+		) => {
+			const mesh = createMesh(color);
+			const pos = getPosition(points[i]);
+			mesh.position.set(...pos);
+			this.scene.add(mesh);
+
+			points[i].pos = pos;
+		});
+
+		points.reduce((a, b) => {
+			this.getCurve(a.pos, b.pos);
+			return b;
+		});
+	}
+
+	getCurve(p1, p2) {
+		const vector1 = new T.Vector3(...p1);
+		const vector2 = new T.Vector3(...p2);
+
+		const pointsArray = [];
+
+		for (let i = 1; i < 30; i += 1) {
+			const p = new T.Vector3().lerpVectors(vector1, vector2, i / 30);
+			p.normalize();
+			p.multiplyScalar(1 + 0.04 * Math.sin((Math.PI * i) / 30));
+
+			pointsArray.push(p);
+		}
+
+		const path = new T.CatmullRomCurve3(pointsArray);
+
+		const geometry = new T.TubeGeometry(path, 20, 0.0025, 8, false);
+		const material = this.shaderMaterial;
+		const mesh = new T.Mesh(geometry, material);
+		this.scene.add(mesh);
 	}
 
 	stop() {
@@ -142,7 +243,7 @@ export default class Sketch {
 	render() {
 		if (!this.isPlaying) return;
 		this.time += 0.05;
-		this.material.uniforms.time.value = this.time;
+		this.shaderMaterial.uniforms.time.value = this.time;
 		window.requestAnimationFrame(this.render.bind(this));
 		this.renderer.render(this.scene, this.camera);
 	}
---
+++ src/static/shader/fragment.glsl
@@ -0,0 +1,7 @@
+varying vec2 vUv;
+uniform float time;
+void main()	{
+	float dash = sin(vUv.x * 50. - time);
+	if (dash < 0.) discard;
+	gl_FragColor = vec4(vUv.x,0.,0.0,1.);
+}
